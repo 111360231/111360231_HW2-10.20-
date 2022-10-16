db52917bf61556ec3c0df60e0a17c7e62836b1f9
111360231 <vivian14053@gmail.com>
2022-10-16 14:48:03 +0800

    FINISH

---
+++ 4.31/source/Main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 1; i <= 9; i = i + 2)
+	{
+		for (k = (9 - i) / 2; k > 0; k--)
+		{
+			printf("%s", " ");
+		}
+		for (j = 1; j <= i; j++)
+		{
+			printf("%s","*");
+		}
+		printf("\n");
+	}
+
+	for (i = 9; i > 0; i = i - 2)
+	{
+		for (k = (9 - i) / 2; k > 0; k--)
+		{
+			printf("%s", " ");
+		}
+		for (j = 1; j <= i; j++)
+		{
+			printf("%s", "*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
